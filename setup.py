0ed930d1b30a60c9cf3a6f26948480cc2607f6ac
Oh Majestic Lama <ohmajesticlama@gmail.com>
2022-01-25 19:11:19 +0100

    Fix installation for PEP517

--- setup.py
+++ setup.py
@@ -1,41 +1,68 @@
-
+"""
+setup file for degiroasync
+"""
 import sys
+import os
 
-def forbid_publish():                                              
-    argv = sys.argv                                                
-    blacklist = ['register', 'upload']                             
-                                                                   
-    for command in blacklist:                                      
-        if command in argv:                                        
+import setuptools
+
+
+# pyproject.toml top
+"""
+[project]
+dependencies = [
+    'httpx >= 0.21.3'
+]
+
+[project.optional-dependencies]
+tests = [
+	'nose2 >= 0.10.0',
+    'mypy >= 0.931'
+]
+"""
+
+def forbid_publish():
+    argv = sys.argv
+    blacklist = ['register', 'upload']
+
+    for command in blacklist:
+        if command in argv:
             print(f'Command "{command}" has been blacklisted, exiting...')
-            sys.exit(2)                                            
+            sys.exit(2)
+
 
 if __name__ == '__main__':
     forbid_publish()  # Not ready for publish
-    import setuptools
-    setuptools.setup()
-    with open("README.md", "r") as fh:
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(readme_path, "r") as fh:
         long_description = fh.read()
-							     
-    setuptools.setup(                                        
-	name="degiroasync",                                      
-	version="0.2",                                       
-	author_email="ohmajesticlama@gmail.com",             
-	description="Python package for Degiro Async API", 
-	long_description=long_description,                   
-	#url="https://github.com/pypa/sampleproject",        
-	scripts=['bin/invaist-report'],                      
-	packages=setuptools.find_packages(),                 
-	install_requires=[                                   
-	    'yfinance>=0.1.64',                              
-	    'degiro-connector>=2.0.13',                      
-	    'pandas>=0.24',                                  
-	    ],                                               
-	classifiers=[                                        
-	    "Programming Language :: Python :: 3",           
-	    "License :: OSI Approved :: BSD License",        
-	    "Operating System :: OS Independent",            
-	],                                                   
-	test_suite='nose2.collector',                         
-	tests_require=['nose2']                               
-    )                                                        
+
+    setuptools.setup(
+        name="degiroasync",
+        version="0.3",
+        author_email="ohmajesticlama@gmail.com",
+        description="A Python asynchronous library for Degiro trading service.",
+        long_description=long_description,
+        #url="https://github.com/pypa/sampleproject",
+        #scripts=[],
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'httpx >= 0.21.3'
+            ],
+        extra_requires={
+            'dev': [
+                'nose2 >= 0.10.0',
+                'mypy >= 0.931',
+                'coverage >= 6.3'
+                'build >= 0.7.0'
+                ]
+            },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        test_suite='nose2.collector',
+        tests_require=['nose2']
+    )
+
