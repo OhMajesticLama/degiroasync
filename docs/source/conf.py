d2513ce9ebf38f6fdd57516b948c2e8d4d7f04e9
Oh Majestic Lama <ohmajesticlama@gmail.com>
2022-04-05 18:40:05 +0200

    Documentation: break line between paramaters.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -64,3 +65,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+        'css/custom.css',
+        ]
