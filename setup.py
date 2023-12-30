c30ca9058ff8db3064e74a8cd9fec1d326dc6afb
OhMajesticLama <ohmajesticlama@gmail.com>
2023-12-30 12:30:49 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name="degiroasync",
-        version="0.21.0",
+        version="0.22.0",
         author_email="ohmajesticlama@gmail.com",
         description=description,
         long_description=long_description,
